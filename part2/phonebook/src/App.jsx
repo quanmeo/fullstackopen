aae896c2ab0b740892a5666f15a06405968657f8
quanmeo <huuquan.learn@gmail.com>
2024-03-04 11:56:50 +0700

    [part2] phonebook 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,34 +1,49 @@
 import { useState } from 'react'
 
 import Header from './components/Header'
-import Names from './components/Names'
+import PhoneBook from './components/PhoneBook'
 
 const App = () => {
-    const [persons, setPersons] = useState(
-        [
-            {
-                name: 'Arto Hellas',
-                id: 1
-            }
-        ]
-    )
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas', number: '040-123456', id: 1 },
+        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    ])
 
     const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
+    const [filter, setFilter] = useState('')
 
-    const onChange = (event) => {
+    const onChangeName = (event) => {
         const nameAdding = event.target.value
         setNewName(nameAdding.trim())
+    }
+    const onChangeNumber = (event) => {
+        const numberAdding = event.target.value
+        setNewNumber(numberAdding.trim())
+    }
+
+    const onChangeFilter = (event) => {
+        const filterValue = event.target.value
+        setFilter(filterValue.trim())
     }
 
     const onAdd = (event) => {
         event.preventDefault()
-        if (persons.find(person => person.name === newName) === undefined) {
+        if (newName === '') {
+            alert('name is empty')
+        } else if (newNumber === '') {
+            alert('number is empty')
+        } else if (persons.find(person => person.name === newName) === undefined) {
             const newPerson = {
                 name: newName,
-                id: persons.length + 1
+                number: newNumber,
+                id: persons.length + 1,
             }
             setPersons(persons.concat(newPerson))
             setNewName('')
+            setNewNumber('')
         } else {
             alert(`${newName} is already added to phonebook`)
         }
@@ -37,9 +52,16 @@ const App = () => {
     return (
         <div>
             <Header text='Phonebook' />
+            <div>
+                filter shown with <input onChange={onChangeFilter} value={filter} />
+            </div>
+            <Header text='add a new' />
             <form onSubmit={onAdd}>
                 <div>
-                    name: <input onChange={onChange} value={newName}/>
+                    name: <input onChange={onChangeName} value={newName} />
+                </div>
+                <div>
+                    number: <input onChange={onChangeNumber} value={newNumber} />
                 </div>
                 <div>
                     <button type='submit'>add</button>
@@ -47,7 +69,9 @@ const App = () => {
                 <div>debug: {newName}</div>
             </form>
             <Header text='Numbers' />
-            <Names persons={persons} />
+            <PhoneBook persons={(filter === '')
+                                ? persons
+                                : persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))} />
         </div>
     )
 }
